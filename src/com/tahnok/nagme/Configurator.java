16b2ec9d79d3ed0a4331c3bc5a8bebc05c6e5cff
Wesley <tahnok881@hotmail.com>
2011-01-15 17:13:21 -0500

    First upload! Still got a lot to fix and I should really build a .apl

---
+++ src/com/tahnok/nagme/Configurator.java
@@ -0,0 +1,88 @@
+package com.tahnok.nagme;
+
+//TODO make the popup using a custom nagging reminder
+//TODO make it remember that custom string
+//TODO remember min, max times
+//TODO make sure min is < max
+
+
+import java.util.Calendar;
+import java.util.Random;
+
+import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class Configurator extends Activity {
+	public static final String TAG = "nagme";
+	
+//	PendingIntent toSend = null;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		
+//		toSend = (PendingIntent) getLastNonConfigurationInstance();
+		
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.configurator);
+		final Button launcher = (Button) findViewById(R.id.startbutton);
+		final EditText min = (EditText) findViewById(R.id.min);
+		final EditText max = (EditText) findViewById(R.id.max);
+
+		launcher.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				int min_time = Integer.parseInt(min.getText().toString());
+				int max_time = Integer.parseInt(max.getText().toString());
+				schedule(min_time, max_time, "Ok fine...");
+			}
+		});
+		final Button cancel = (Button) findViewById(R.id.stopbutton);
+		cancel.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				cancelPending();
+				
+			}
+		});
+	}
+	
+	public void schedule(int min, int max, String message){
+		cancelPending();
+		AlarmManager mgr= (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+		
+		Intent i = new Intent(getApplicationContext(), StartReciever.class);
+		i.putExtra("min", min);
+		i.putExtra("max", max);
+		i.putExtra("message", message);
+		PendingIntent toSend = PendingIntent.getBroadcast(getApplicationContext(), 1123498, i, PendingIntent.FLAG_CANCEL_CURRENT);
+		
+		Calendar cal = Calendar.getInstance();
+		Random rand = new Random();
+		int next = rand.nextInt(max-min) + min;
+		cal.add(Calendar.MINUTE, next);
+		mgr.set(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), toSend);
+		Log.d(TAG, "Event scheduled");
+	}
+	
+	public void cancelPending(){
+		AlarmManager mgr= (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+		Intent i = new Intent(getApplicationContext(), StartReciever.class);
+		PendingIntent toSend = PendingIntent.getBroadcast(getApplicationContext(), 1123498, i, PendingIntent.FLAG_CANCEL_CURRENT);
+		mgr.cancel(toSend);
+	}
+	
+	
+	
+}
---
+++ src/com/tahnok/nagme/Popup.java
@@ -0,0 +1,72 @@
+package com.tahnok.nagme;
+
+import java.util.Calendar;
+import java.util.Random;
+import com.tahnok.nagme.R;
+import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Vibrator;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+
+public class Popup extends Activity {
+    /** Called when the activity is first created. */
+	
+	public static final String TAG = "nagme";
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+    	Bundle bundle = getIntent().getExtras();
+        super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.popup);
+		 final String msg = bundle.getString("message");
+		 final int min = bundle.getInt("min");
+		 final int max = bundle.getInt("max");
+		
+		final Button testButton = (Button) findViewById(R.id.DismissButton);
+		
+		testButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+		    	Vibrator h = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+		    	h.cancel();
+		    	schedule(min,max,msg);
+				finish();
+			}
+		});
+        vibrate();
+    }
+    
+    public void vibrate(){
+    	Log.d(TAG, "now here");
+    	Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+    	long[] pattern = {0,200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 1000};
+    	v.vibrate(pattern, -1);
+    
+    }
+    
+    public void schedule(int min, int max, String message){
+		AlarmManager mgr= (AlarmManager) getSystemService(Context.ALARM_SERVICE);		
+		Intent i = new Intent(getApplicationContext(), StartReciever.class);
+		i.putExtra("min", min);
+		i.putExtra("max", max);
+		i.putExtra("message", message);
+		PendingIntent toSend = PendingIntent.getBroadcast(getApplicationContext(), 1123498, i, PendingIntent.FLAG_CANCEL_CURRENT);
+		
+		Calendar cal = Calendar.getInstance();
+		Random rand = new Random();
+		int next = rand.nextInt(max-min) + min;
+		cal.add(Calendar.MINUTE, next);
+		mgr.set(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), toSend);
+		Log.d(TAG, "Event scheduled");
+	}
+    
+}
